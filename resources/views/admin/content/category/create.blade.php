88f929bb564b179ff0b6d6dd48c7f2e0a5c68783
omid zamani <omid.zamani4113367975@gmail.com>
2022-01-29 18:33:51 +0330

    validation and store data in database

--- resources/views/admin/content/category/create.blade.php
+++ resources/views/admin/content/category/create.blade.php
@@ -30,26 +30,86 @@
             </section>
 
             <section>
-                <form action="" method="">
+                <form action="{{ route('admin.content.category.store') }}" method="post" enctype="multipart/form-data">
+                    @csrf
                     <section class="row">
 
-                        <section class="col-12 col-md-6">
+                        <section class="col-12 col-md-6 my-2">
                             <div class="form-group">
-                                <label for="">نام دسته</label>
-                                <input type="text" class="form-control form-control-sm">
+                                <label for="name">نام دسته</label>
+                                <input type="text" class="form-control form-control-sm" name="name" id="name" value="{{old('name')}}">
                             </div>
+                            @error('name')
+                                <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                    <strong>
+                                        {{$message}}
+                                    </strong>
+                                </span>
+                            @enderror
                         </section>
 
-                        <section class="col-12 col-md-6">
+                        <section class="col-12 col-md-6 my-2">
                             <div class="form-group">
-                                <label for="">دسته والد</label>
-                                <select name="" id="" class="form-control form-control-sm">
-                                    <option value="">دسته را انتخاب کنید</option>
-                                    <option value="">وسایل الکترونیکی</option>
+                                <label for="tags">تگ ها</label>
+                                <input type="text" class="form-control form-control-sm" name="tags" id="tags" value="{{old('tags')}}">
+                            </div>
+                            @error('tags')
+                                <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                    <strong>
+                                        {{$message}}
+                                    </strong>
+                                </span>
+                            @enderror
+                        </section>
+
+                        <section class="col-12 col-md-6 my-2">
+                            <div class="form-group">
+                                <label for="status">وضعیت</label>
+                                <select name="status" id="status" class="form-control form-control-sm">
+                                    <option value="0" @if(old('status')==0) selected @endif>غیرفعال</option>
+                                    <option value="1" @if(old('status')==1) selected @endif>فعال</option>
                                 </select>
                             </div>
+                            @error('status')
+                                <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                    <strong>
+                                        {{$message}}
+                                    </strong>
+                                </span>
+                            @enderror
                         </section>
-                        <section class="col-12">
+
+                        <section class="col-12 col-md-6 my-2">
+                            <div class="form-group">
+                                <label for="image">تصویر</label>
+                                <input type="file" class="form-control form-control-sm" name="image" id="image">
+                            </div>
+                            @error('image')
+                                <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                    <strong>
+                                        {{$message}}
+                                    </strong>
+                                </span>
+                            @enderror
+                        </section>
+
+                        <section class="col-12 my-2">
+                            <div class="form-group">
+                                <label for="description">توضیحات</label>
+                                <textarea name="description" id="description"  class="form-control form-control-sm" rows="6">
+                                    {{old('description')}}
+                                </textarea>
+                            </div>
+                            @error('description')
+                                <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                    <strong>
+                                        {{$message}}
+                                    </strong>
+                                </span>
+                            @enderror
+                        </section>
+
+                        <section class="col-12 my-3">
                             <button class="btn btn-primary btn-sm">ثبت</button>
                         </section>
                     </section>
@@ -59,5 +119,13 @@
         </section>
     </section>
 </section>
+
+@endsection
+@section('script')
+
+    <script src="{{ asset('admin-assets/ckeditor/ckeditor.js') }}"></script>
+    <script>
+        CKEDITOR.replace('description');
+    </script>
 
 @endsection
