74e61037b6f65e9bae3fbf6d3db4a047e3b35ca5
omid zamani <omid.zamani4113367975@gmail.com>
2022-01-30 13:43:36 +0330

    complete select2,change status using ajax

--- resources/views/admin/content/category/index.blade.php
+++ resources/views/admin/content/category/index.blade.php
@@ -57,7 +57,8 @@
                             <td>{{ $postCategory->tags }}</td>
                             <td>
                                 <label>
-                                    <input type="checkbox" @if ($postCategory->status===1)
+                                    <input id="{{ $postCategory->id }}" onchange="changeStatus({{ $postCategory->id }})" data-url="{{ route('admin.content.category.status',$postCategory->id) }}"
+                                     type="checkbox" @if ($postCategory->status===1)
                                     checked
                                     @endif
                                     >
@@ -83,4 +84,29 @@
     </section>
 </section>
 
+@endsection
+
+@section('script')
+<script type="text/javascript">
+    function changeStatus(id){
+        var element=$('#'+id)
+        var url=element.attr('data-url');
+        var elementValue=!element.prop('checked');
+        $.ajax({
+            url:url,
+            type:"GET",
+            success:function(response){
+                if(response.status){
+                    if(response.checked)
+                        element.prop('checked',true);
+                    else
+                        element.prop('checked',false);
+                }
+                else{
+                    element.prop('checked',elementValue)
+                }
+            }
+        })
+    }
+</script>
 @endsection
