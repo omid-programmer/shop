e029f72572b25d0d3b3aab16cfba98a17703d62f
omid zamani <omid.zamani4113367975@gmail.com>
2022-03-06 17:11:55 +0330

    show order factor

--- app/Models/Market/Order.php
+++ app/Models/Market/Order.php
@@ -2,6 +2,8 @@
 
 namespace App\Models\Market;
 
+use App\Models\User;
+use App\Models\Address;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -17,5 +19,21 @@ class Order extends Model
      public function delivery()
     {
         return $this->belongsTo(Delivery::class);
+    }
+      public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+        public function address()
+    {
+        return $this->belongsTo(Address::class);
+    }
+        public function copan()
+    {
+        return $this->belongsTo(Copan::class);
+    }
+          public function commonDiscount()
+    {
+        return $this->belongsTo(CommonDiscount::class);
     }
 }
