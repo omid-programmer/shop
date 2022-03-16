80e8020298d04d9c7e4729a81736c68d47b440ef
omid zamani <omid.zamani4113367975@gmail.com>
2022-03-16 14:40:30 +0330

    add layouts of main page

--- routes/web.php
+++ routes/web.php
@@ -414,6 +414,10 @@ Route::prefix('admin')->namespace('Admin')->group(function () {
     Route::post('/notification/read-all', [NotificationController::class, 'readAll'])->name('admin.notification.readAll');
 });
 
+Route::get('/', function (){
+    return view('customer.home');
+})->name('customer.home');
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
