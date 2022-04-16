a3a0bcd72840c319bef608c0af62f0b9daa2d022
omid zamani <omid.zamani4113367975@gmail.com>
2022-04-17 04:03:44 +0430

    confirm and verify email and phone

--- app/Http/Requests/Auth/Customer/LoginRegisterRequest.php
+++ app/Http/Requests/Auth/Customer/LoginRegisterRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Auth\Customer;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Http\FormRequest;
 
 class LoginRegisterRequest extends FormRequest
@@ -23,9 +24,20 @@ class LoginRegisterRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-           'id' => 'required|min:11|max:64|regex:/^[a-zA-Z0-9_.@\+]*$/',
-        ];
+        $route = Route::current();
+        if($route->getName() == 'auth.customer.login-register')
+        {
+            return [
+                'id' => 'required|min:11|max:64|regex:/^[a-zA-Z0-9_.@\+]*$/',
+             ];
+        }
+        elseif($route->getName() == 'auth.customer.login-confirm')
+        {
+            return [
+                'otp' => 'required|min:6|max:6',
+             ];
+        }
+
     }
 
 
--- resources/lang/fa/validation.php
+++ resources/lang/fa/validation.php
@@ -189,6 +189,6 @@ return [
         'discount_ceiling' => 'سقف تخفیف',
         'minimal_order_amount' => 'حداقل مبلغ',
         'product_id' => 'کالا',
-
+        'otp'=>'رمز یک بار مصرف'
     ],
 ];
