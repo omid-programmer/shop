7969bc6fece14106e5377f1f6ba0d546cb87d13b
omid zamani <omid.zamani4113367975@gmail.com>
2022-04-24 23:55:28 +0430

    resend otp code

--- resources/views/customer/auth/login-confirm.blade.php
+++ resources/views/customer/auth/login-confirm.blade.php
@@ -1,5 +1,15 @@
 @extends('customer.layouts.master-simple')
 
+@section('head-tag')
+
+<style>
+    #resend-otp{
+        font-size: 1rem;
+    }
+</style>
+
+@endsection
+
 @section('content')
 
 
@@ -39,9 +49,60 @@
         @enderror
         </section>
         <section class="login-btn d-grid g-2"><button class="btn btn-danger">تایید</button></section>
+
+        <section id="resend-otp" class="d-none">
+            <a href="{{ route('auth.customer.login-resend-otp', $token) }}" class="text-decoration-none text-primary">دریافت مجدد کد تایید</a>
+        </section>
+        <section id="timer"></section>
+
     </section>
     </form>
 </section>
 
+
+@endsection
+
+
+@section('script')
+
+@php
+    $timer = ((new \Carbon\Carbon($otp->created_at))->addMinutes(5)->timestamp - \Carbon\Carbon::now()->timestamp) * 1000;
+@endphp
+
+<script>
+
+   var countDownDate = new Date().getTime() + {{ $timer }};
+    var timer = $('#timer');
+    var resendOtp = $('#resend-otp');
+
+    var x = setInterval(function(){
+
+        var now = new Date().getTime();
+
+        var distance = countDownDate - now;
+
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        if(minutes == 0){
+            timer.html('ارسال مجدد کد تایید تا ' + seconds + 'ثانیه دیگر')
+        }
+        else{
+            timer.html('ارسال مجدد کد تایید تا ' + minutes + 'دقیقه و ' + seconds + 'ثانیه دیگر');
+        }
+        if(distance < 0)
+        {
+            clearInterval(x);
+            timer.addClass('d-none');
+            resendOtp.removeClass('d-none');
+        }
+
+    }, 1000)
+
+
+
+
+
+</script>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -416,11 +416,14 @@ Route::prefix('admin')->namespace('Admin')->group(function () {
 });
 
 
+
+
 Route::namespace('Auth')->group(function () {
     Route::get('login-register', [LoginRegisterController::class, 'loginRegisterForm'])->name('auth.customer.login-register-form');
     Route::post('/login-register', [LoginRegisterController::class, 'loginRegister'])->name('auth.customer.login-register');
     Route::get('login-confirm/{token}', [LoginRegisterController::class, 'loginConfirmForm'])->name('auth.customer.login-confirm-form');
     Route::post('/login-confirm/{token}', [LoginRegisterController::class, 'loginConfirm'])->name('auth.customer.login-confirm');
+    Route::get('/login-resend-otp/{token}', [LoginRegisterController::class, 'loginResendOtp'])->name('auth.customer.login-resend-otp');
  });
  
 Route::get('/', function (){
